59afc9f78b8999354b02fe7e708fa9e3245d6e94
Parker Gabel <parker.d.gabel@gmail.com>
2021-10-11 17:46:35 -0700

    added test

--- test/project_euler/problems/problem_6_test.clj
+++ test/project_euler/problems/problem_6_test.clj
@@ -1,1 +1,7 @@
-(ns project-euler.problems.problem-6-test)
+(ns project-euler.problems.problem-6-test
+  (:require [clojure.test :refer :all]
+            [project-euler.problems.problem-6 :refer [solution]]))
+
+(deftest problem-6-answer
+  (testing "The correct answer returned for problem 6"
+    (is (= 25164150 solution))))
