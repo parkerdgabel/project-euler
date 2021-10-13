2e01ca099b77626682164e3df6696121bf8ca41b
Parker Gabel <parker.d.gabel@gmail.com>
2021-10-13 15:56:42 -0700

    Finished 8

---
+++ test/project_euler/problems/problem_8_test.clj
@@ -0,0 +1,7 @@
+(ns project-euler.problems.problem-8-test
+  (:require [clojure.test :refer :all]
+            [project-euler.problems.problem-8 :refer [solution]]))
+
+(deftest problem-6-answer
+  (testing "The correct answer returned for problem 6"
+    (is (= 23514624000 solution))))
