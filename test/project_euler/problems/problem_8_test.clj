4dd9a39914e56f480734a262568ed3d7288bc137
Parker Gabel <parker.d.gabel@gmail.com>
2021-10-23 17:01:36 -0700

    Finished 9

--- test/project_euler/problems/problem_8_test.clj
+++ test/project_euler/problems/problem_8_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [project-euler.problems.problem-8 :refer [solution]]))
 
-(deftest problem-6-answer
+(deftest problem-8-answer
   (testing "The correct answer returned for problem 6"
     (is (= 23514624000 solution))))
--- test/project_euler/problems/problem_9_test.clj
+++ test/project_euler/problems/problem_9_test.clj
@@ -1,1 +1,7 @@
-(ns project-euler.problems.problem-9-test)
+(ns project-euler.problems.problem-9-test
+  (:require [clojure.test :refer :all]
+            [project-euler.problems.problem-9 :refer [solution]]))
+
+(deftest problem-8-answer
+  (testing "The correct answer returned for problem 6"
+    (is (= 31875000 solution))))
